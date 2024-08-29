1971181f6a6ce0f00d4a08f46885628a828cfb56
Fernando Campos <fernandostcampos@hotmail.com>
2024-08-29 12:11:30 +0100

    Getting article by id sorted

--- server/app.js
+++ server/app.js
@@ -1,41 +1,23 @@
 const express = require("express");
-const {
-  getAllTopics,
-  getArticleById,
-} = require("./controllers/nc_news_controller");
+const { getAllTopics, getArticleById } = require("./controllers/nc_news_controller");
 const endpoints = require("../endpoints.json");
 const app = express();
 
 app.use(express.json());
 
+// Routes
 app.get("/api/topics", getAllTopics);
-
-app.use((err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    res.status(500).send({ msg: "Internal Server Error" });
-  }
-});
-
 app.get("/api", (req, res) => {
   res.status(200).send(endpoints);
 });
-
-app.use("/api", (err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    res.status(500).send({ msg: "Internal Server Error" });
-  }
-});
-
 app.get("/api/articles/:article_id", getArticleById);
 
+// General error handling middleware
 app.use((err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ msg: err.msg });
   } else {
+    console.log(err);
     res.status(500).send({ msg: "Internal Server Error" });
   }
 });
--- server/models/nc_news_model.js
+++ server/models/nc_news_model.js
@@ -8,11 +8,15 @@ exports.fetchAllTopics = () => {
 
 exports.fetchArticleByID = (articleId) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [articleId])
+    .query(
+      "SELECT  author, title, article_id, body, topic, created_at, votes, article_img_url FROM articles WHERE article_id = $1",
+      [articleId]
+    )
     .then((result) => {
       if (result.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Article not found" });
       }
+
       return result.rows[0];
     });
 };
