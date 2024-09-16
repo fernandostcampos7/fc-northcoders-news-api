e6617870f61dc9bfd741b5b02faa71a02460b905
Fernando Campos <fernandostcampos@hotmail.com>
2024-09-16 16:58:27 +0100

    Supabase configuration sorted

--- db/connection.js
+++ db/connection.js
@@ -6,13 +6,13 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE /*&& !process.env.DATABASE_URL*/) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
 
-// if (ENV === "production") {
-//   config.connectionString = process.env.DATABASE_URL;
-//   config.max = 2;
-// }
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
-module.exports = new Pool();
+module.exports = new Pool(config);
